daafab5a445ac915a44d2889dbb41df76d6427db
unknown <junqiu>
2020-04-28 14:06:07 +0800

    商品详情增加底部按钮

--- src/pages/shopping-mall/GoodsDetailPage.vue
+++ src/pages/shopping-mall/GoodsDetailPage.vue
@@ -1,11 +1,17 @@
 <template>
-  <page :has-header="true" :has-footer="false">
+  <page :has-header="showNav" :has-footer="false">
     <template slot="header">
       <NavBar title="商品详情" :show-left="true" />
     </template>
 
     <template slot="content">
-
+      <van-goods-action>
+        <van-goods-action-icon icon="chat-o" text="客服" dot />
+        <van-goods-action-icon icon="cart-o" text="购物车" info="5" />
+        <van-goods-action-icon icon="shop-o" text="店铺" badge="12" />
+        <van-goods-action-button type="warning" text="加入购物车" />
+        <van-goods-action-button type="danger" text="立即购买" />
+      </van-goods-action>
     </template>
 
   </page>
@@ -13,6 +19,7 @@
 
 <script>
   import NavBar from '../../components/NavBar.vue';
+  import {utils} from "../../utils/utils";
   export default {
     name: "GoodsDetailPage",
     components:{
@@ -23,7 +30,7 @@
     },
     data() {
       return {
-
+        showNav:!utils.isWeChat()
       }
     },
     created() {
@@ -35,6 +42,6 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 
 </style>
